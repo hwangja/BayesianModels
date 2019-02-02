5d6cee5523bd4f3d576119ef06175cd7a9ebb4e6
James Hwang <jameshwang@outlook.com>
2019-02-02 15:10:40 +0000

    Added example python example: poisson distribution on matplotlib
    source: https://stackoverflow.com/questions/51242748/plot-a-poisson-distribution-graph-in-python

---
+++ example/sample-poisson.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+t = np.arange(0, 20, 0.1)
+d = []
+
+for i in t:
+    p = pow(5,i)
+    q = p/math.factorial(i)
+    d.append(q)
+
+plt.plot( t, np.exp(5)*d, 'bs')
+plt.show()
