65676df58c8f8243eec821cb3f6851ee8202fba4
Daniel Weingard <DanielWeingard@gmail.com>
2019-02-09 18:42:17 +0000

    added some comments

--- ugen/ugen.py
+++ ugen/ugen.py
@@ -7,9 +7,35 @@ def polynomial_truncated_multiplication(polynomial_0, polynomial_1, term_count):
     return updated_polynomial
 
 def makeJ(j, m):
+    '''
+    make an array of 1, j, j^2,..., j^m
+    
+    inputs
+    j: int
+       product iterator
+    m: int
+       number of terms kept
+    
+    Returns
+    J: array
+       [1, j, j^2,..., j^m]
+    '''
     return np.power(j,range(m+1))
 
 def generate_u_constants(c, m):
+    '''
+    make Uis from paper
+    
+    inputs
+    c: int
+       number of terms multiplied
+    m: int
+       number of terms kept
+       
+    Returns
+    U: array
+       Array containing U's from paper
+    '''
     U = makeJ(2, m)
     for i in range(c+1)[3:]:
         U = U
