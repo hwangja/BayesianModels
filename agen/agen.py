a50e750d3ef0a7e9d9c457e7f4f5af534fa6fa2d
Allen Yu <my1826@nyu.edu>
2019-02-23 16:47:49 +0000

    added generate_a()

---
+++ agen/agen.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numpy.polynomial import polynomial as P
+
+def generate_a(H):
+    '''
+    input:
+    H: exponential terms for the polynomial
+    
+    output:
+    A: polynomial coefficients
+    '''
+    
+    A = [1]
+    for j, h in enumerate(H):
+        for i in range(h):
+            A = P.polymul(A, [1,j])
+    return A
+
+#testing    
+H = [x for x in range(50)][::-1]
+print(H)
+A = generate_a(H)
+print(A)
+print(len(A))
